c1bdd78beae2b49916ac445342544a8ba0c76e69
Juan Davila <juan23davila@gmail.com>
2025-02-17 19:10:50 -0500

    optimizing code

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -1,67 +1,48 @@
 import { test, expect } from '@playwright/test';
 
 test('book a class', async ({ browser }) => {
-  const sede = "Aves María", hour = "05", minute = "10", day="MAR"
+  const sede = "Aves María"; 
+  const hour = "05"; 
+  const minute = "10"; 
+  const day="MAR";
+
+  // Diccionario para obtener el índice basado en el día
+  const dayIndex = {
+    'DOM': 1,
+    'LUN': 2,
+    'MAR': 3,
+    'MIE': 4,
+    'JUE': 5,
+    'VIE': 6,
+    'SAB': 7
+  };
+
+  // 1. Create context and page
   const context = await browser.newContext()
   const page = await context.newPage()
+
+  // 2. Navigate and open Sedes list
   await page.goto('/reservas');
   await page.locator(".reservas_select__YsLUn").click()
 
-  // code to print list of clubs
-  /*const clubes = await page.locator("//ul[@class = 'reservas_list_clubs__koaW6']/li").all()
-  console.log(clubes)
-  for(let e of clubes){
-    const club = await e.textContent();
-    console.log(club)
-  }*/
-  //page.getByText("Aves María").click()
-
-  // En tu test
+  // 3. Capture new tab after click in sede opt
   const [page2] = await Promise.all([
-    context.waitForEvent('page'),        // Esperar a que se cree la nueva página
+    context.waitForEvent('page'), // Esperar a que se cree la nueva página
     page.getByText(sede).click() // Evento que dispara la creación de la nueva pestaña
   ]);
-
-  // Recomendado: esperar a que la nueva pestaña cargue
   await page2.waitForLoadState('networkidle');
 
-  /*const rowsPerHour = await page2.locator("//span[@class='texto-horario' and text()="+hour+"]").all()
-  for(let rowPerHour of rowsPerHour){
-    const row = await rowPerHour.textContent();
-    console.log(row)
-  }*/
+  // 4. Locate train session
+  const locatorTrain = page2.locator(`
+    xpath = 
+      //span[@class='texto-horario' and text()='${hour}']
+      /following-sibling::div
+      /span[text()='${minute}']
+      /ancestor::div[@class='container-grade']
+      /div[${dayIndex[day]}]
+  `);
 
-  const minuteOfClass = await page2.locator("//span[@class='texto-horario' and text()="+hour+"]/following-sibling::div/span[text()="+minute+"]")
-  //console.log(await minuteOfClass.textContent())
-
-  const classes = await minuteOfClass.locator("xpath=/parent::div/parent::div/parent::div[@class='container-grade']/div").all()
-  /*for(let train of classes){
-    const infoTrain = await train.textContent();
-    console.log(infoTrain)
-  }*/
-  let train
-  switch (day) {
-    case "DOM":
-      train = classes[1]
-      break;
-
-    case "LUN":
-    train = classes[2]
-    break;
-
-    case "MAR":
-      train = classes[3]
-      break;
-
-    case "MIE":
-    train = classes[4]
-    break;
-  
-    default:
-      break;
-  }
-
-  console.log(await train.textContent())
+  console.log(await locatorTrain.textContent())
 
   await context.close()
 });
